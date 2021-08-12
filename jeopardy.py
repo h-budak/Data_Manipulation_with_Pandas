9733e0950ddc1f8ee618bc471176cfd562c7c7e9
h-budak <bullkaop@gmail.com>
2021-08-11 23:36:22 -0500

    Renaming misformatted columns

--- jeopardy.py
+++ jeopardy.py
@@ -1,4 +1,9 @@
 import pandas as pd
 # Loading the data and investigating it
 jeopardy_data = pd.read_csv("jeopardy.csv")
-print(jeopardy_data.columns)
+#print(jeopardy_data.columns)
+
+# Renaming misformatted columns
+jeopardy_data = jeopardy_data.rename(columns = {" Air Date": "Air Date", " Round" : "Round", " Category": "Category", " Value": "Value", " Question":"Question", " Answer": "Answer"})
+#print(jeopardy_data.columns)
+#print(jeopardy_data["Question"])
